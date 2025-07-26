312d92642ea6d8ab1d10a7b5201e563bb538f4f5
Ishant Choudhary <codeitishant@gmail.com>
2025-07-26 15:51:31 +0530

    fixed path errors

--- frontend/client/tailwind.config.ts
+++ frontend/client/tailwind.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "../shared/**/*.{ts,tsx,js,jsx}"
+  ],
   theme: {
     extend: {
       borderRadius: {
@@ -86,5 +92,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-} satisfies Config;
+  plugins: [tailwindcssAnimate, typography],
+} satisfies Config; 
