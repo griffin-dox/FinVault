afe6a83b4d1f0e54ec1dc50edef3b2acbf8e8622
Ishant Choudhary <codeitishant@gmail.com>
2025-08-31 01:01:57 +0530

    refx

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import NotFound from "@/pages/not-found";
@@ -49,6 +50,7 @@ function App() {
         <TooltipProvider>
           <AuthProvider>
             <Router base="/">
+              <Toaster /> 
               <AppRouter />
             </Router>
           </AuthProvider>
